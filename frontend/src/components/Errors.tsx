354676d487fb1567ef3ea782f68fac715bfd76ce
Sergey Ulanov <ulanovsega@gmail.com>
2024-05-11 19:47:02 +0300

    mini-edit frontend

--- frontend/src/components/Errors.tsx
+++ frontend/src/components/Errors.tsx
@@ -1,6 +1,6 @@
 export function Error500() {
     return (
-        <div className="mx-auto bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="mx-auto mt-20 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Internal Server Error 500
@@ -13,7 +13,7 @@ export function Error500() {
 
 export function Error403() {
     return (
-        <div className="mx-auto bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="mx-auto mt-20 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Forbidden 403
